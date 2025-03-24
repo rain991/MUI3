12d6844f943fabbde99677f17a5882af37bc2886
IvanSavenko <savenkoivan931@gmail.com>
2025-03-24 21:09:59 +0100

    miscellaneous corrections

--- app/src/main/java/com/example/mui3/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mui3/presentation/MainActivity.kt
@@ -3,15 +3,17 @@ package com.example.mui3.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import com.example.mui3.presentation.theme.MUI3Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +22,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             MUI3Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center){
-                        Text(text = "Numeral Systems", style = MaterialTheme.typography.headlineMedium)
+                    Column(modifier = Modifier.padding(innerPadding)){
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Text(
+                                text = "Numeral Systems",
+                                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.SemiBold),
+                            )
+                        }
+                        NumberSystemConverter(innerPadding)
                     }
-                    NumberSystemConverter(innerPadding)
                 }
             }
         }
--- app/src/main/java/com/example/mui3/presentation/NumeralSystemScreen.kt
+++ app/src/main/java/com/example/mui3/presentation/NumeralSystemScreen.kt
@@ -1,11 +1,15 @@
 package com.example.mui3.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,7 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun NumberSystemConverter(innerPadding: PaddingValues) {
@@ -40,7 +45,11 @@ fun NumberSystemConverter(innerPadding: PaddingValues) {
         decValue?.let { updateFromDecimal(it.toString()) }
     }
 
-    Column(modifier = Modifier.padding(innerPadding), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        modifier = Modifier.padding(innerPadding),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
         NumericSystemField("Binary", binary, { handleInput(it, 2) })
         NumericSystemField("Octal", octal, { handleInput(it, 8) })
         NumericSystemField("Decimal", decimal, { handleInput(it, 10) })
@@ -50,13 +59,22 @@ fun NumberSystemConverter(innerPadding: PaddingValues) {
 
 @Composable
 fun NumericSystemField(label: String, value: String, onValueChange: (String) -> Unit) {
-    Column {
-        Text(text = label, fontWeight = FontWeight.SemiBold)
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(4.dp))
         BasicTextField(
             value = value,
             onValueChange = onValueChange,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.CenterHorizontally),
+            textStyle = MaterialTheme.typography.titleLarge.copy(textAlign = TextAlign.Center)
         )
+        Spacer(modifier = Modifier.height(4.dp))
         HorizontalDivider(
             modifier = Modifier
                 .fillMaxWidth(0.65f)
